98f87e282fc00815073e0694e05e357816557166
Muhammad Muneeb <47159080+MuhammadMuneeb007@users.noreply.github.com>
2024-05-26 21:54:46 +1000

    Add files via upload

---
+++ Method2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=kathydata
+#SBATCH --nodes=1
+#SBATCH --partition=ascher
+#SBATCH --time=72:00:00
+#SBATCH --output=job.%A_%a.out
+#SBATCH --error=job.%A_%a.err
+#SBATCH --array=1-55
+
+radius1=$(head -n $SLURM_ARRAY_TASK_ID PhenotypeParameters.txt | tail -n 1)
+radius2=$(head -n $SLURM_ARRAY_TASK_ID FoldParameters.txt | tail -n 1)
+
+python Method2.py $radius1 $radius2
+
+
+
